adb980e10fd556b9de789608e7e6f1c31f719a26
Oksil-Joo <bbkke07@gmail.com>
2022-01-28 14:55:55 -0500

    updatePHPfile

---
+++ includes/partials/navigation.php
@@ -0,0 +1,7 @@
+<nav>
+     <ul>
+        <li> <a href="index.php"> HOME </a></li>
+        <li> <a href="listing.php"> LISTING </a></li>
+        <li> <a href="contact.php"> CONTACT ME </a></li>
+    </ul>
+</nav>
--- public/index.php
+++ public/index.php
@@ -1,3 +1,8 @@
+<?php
+session_start();
+
+include '../includes/functions.php';
+?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -8,6 +13,37 @@
     <title>Multi-page PHP site</title>
 </head>
 <body>
-
+    <header id="container">
+        <h1 class="hidden">logo</h1>
+        <section id="top">
+                <?php
+                include '../includes/partials/headers.php'
+                ?>
+            <div id="navigation">
+                <?php
+                include '../includes/partials/navigation.php'
+                ?>
+            </div>
+        </section>
+    </header>
+    <main>
+        <section class="mainCon">
+            <h1 class="hidden">mainTitle</h1>
+            <div class="open">
+                <img src="images/open.svg" alt="open">
+            </div>
+            <div class="title">
+                <p class="role">UI/US Designer</p>
+                <p class="name">OKSIL JOO</p>
+                <p class="role">PORTFOLIO</p>
+            </div>
+            <div class="open">
+                <img src="images/close.svg" alt="open">
+            </div>
+        </section>
+    </main>
+    <?php
+     include '../includes/partials/footers.php'
+     ?>
 </body>
 </html>
